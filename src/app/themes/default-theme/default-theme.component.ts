d0f691d983f5c0dc0367b5564934675603a7cead
Pradeepsuthar <sutharpradeep081@gmail.com>
2020-08-14 10:14:39 +0000

    add firebase

--- src/app/themes/default-theme/default-theme.component.ts
+++ src/app/themes/default-theme/default-theme.component.ts
@@ -7,6 +7,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DefaultThemeComponent implements OnInit {
 
+  products:any=[
+    {
+      productImgUrl:"https://pradeepsuthar.github.io/hesley_website_australia/assets/images/placeholder-200x200.png",
+      productName:"Product name 1",
+      sale:true,
+      mrpPrice:399,
+      salePrice:299
+    },
+    {
+      productImgUrl:"https://pradeepsuthar.github.io/hesley_website_australia/assets/images/placeholder-200x200.png",
+      productName:"Product name 2",
+      sale:true,
+      mrpPrice:289,
+      salePrice:199
+    },
+    {
+      productImgUrl:"https://pradeepsuthar.github.io/hesley_website_australia/assets/images/placeholder-200x200.png",
+      productName:"Product name 3",
+      sale:true,
+      mrpPrice:799,
+      salePrice:499
+    },
+  ]
+
+  photoGallry:any=[
+    {
+      imgUrl:"https://pradeepsuthar.github.io/hesley_website_australia/assets/images/placeholder-200x200.png"
+    },
+    {
+      imgUrl:"https://pradeepsuthar.github.io/hesley_website_australia/assets/images/placeholder-200x200.png"
+    },
+    {
+      imgUrl:"https://pradeepsuthar.github.io/hesley_website_australia/assets/images/placeholder-200x200.png"
+    },
+    {
+      imgUrl:"https://pradeepsuthar.github.io/hesley_website_australia/assets/images/placeholder-200x200.png"
+    },
+    {
+      imgUrl:"https://pradeepsuthar.github.io/hesley_website_australia/assets/images/placeholder-200x200.png"
+    },
+    {
+      imgUrl:"https://pradeepsuthar.github.io/hesley_website_australia/assets/images/placeholder-200x200.png"
+    },
+    {
+      imgUrl:"https://pradeepsuthar.github.io/hesley_website_australia/assets/images/placeholder-200x200.png"
+    },
+    {
+      imgUrl:"https://pradeepsuthar.github.io/hesley_website_australia/assets/images/placeholder-200x200.png"
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAJOm29FsjGtNiuj8g3m3G3S5iW5n69Oig",
+    authDomain: "webbuilder081.firebaseapp.com",
+    databaseURL: "https://webbuilder081.firebaseio.com",
+    projectId: "webbuilder081",
+    storageBucket: "webbuilder081.appspot.com",
+    messagingSenderId: "142256510593",
+    appId: "1:142256510593:web:8c1a07c0c50c3f31b1df80",
+    measurementId: "G-2RR8FHSQL3"
+  }
 };
 
 /*
