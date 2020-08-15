b47350bb04cbe1de7ddd86665bf06fb2e2959b0b
Pradeepsuthar <sutharpradeep081@gmail.com>
2020-08-15 09:52:11 +0000

    changes done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,7 +30,7 @@ import {GoogleMapsModule} from '@angular/google-maps';
     AngularFirestoreModule,
     YouTubePlayerModule,
     GoogleMapsModule,
-    
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/themes/default-theme/default-theme.component.ts
+++ src/app/themes/default-theme/default-theme.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemesManagerService } from 'src/app/services/themes-manager.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-default-theme',
@@ -7,66 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DefaultThemeComponent implements OnInit {
   // map vars start
-  center = {lat: 24, lng: 12};
-  markerOptions = {draggable: false};
-  markerPositions: google.maps.LatLngLiteral[] = [{lat:24,lng:12}];
+  center = { lat: 24, lng: 12 };
+  markerOptions = { draggable: false };
+  markerPositions: google.maps.LatLngLiteral[] = [{ lat: 24, lng: 12 }];
   zoom = 4;
   display?: google.maps.LatLngLiteral;
   // map vars end
 
-  products:any=[
-    {
-      productImgUrl:"https://pradeepsuthar.github.io/hesley_website_australia/assets/images/placeholder-200x200.png",
-      productName:"Product name 1",
-      sale:true,
-      mrpPrice:399,
-      salePrice:299
-    },
-    {
-      productImgUrl:"https://pradeepsuthar.github.io/hesley_website_australia/assets/images/placeholder-200x200.png",
-      productName:"Product name 2",
-      sale:true,
-      mrpPrice:289,
-      salePrice:199
-    },
-    {
-      productImgUrl:"https://pradeepsuthar.github.io/hesley_website_australia/assets/images/placeholder-200x200.png",
-      productName:"Product name 3",
-      sale:true,
-      mrpPrice:799,
-      salePrice:499
-    },
-  ]
+  constructor(
+    public _themeService: ThemesManagerService
+  ) { }
 
-  photoGallry:any=[
-    {
-      imgUrl:"https://pradeepsuthar.github.io/hesley_website_australia/assets/images/placeholder-200x200.png"
-    },
-    {
-      imgUrl:"https://pradeepsuthar.github.io/hesley_website_australia/assets/images/placeholder-200x200.png"
-    },
-    {
-      imgUrl:"https://pradeepsuthar.github.io/hesley_website_australia/assets/images/placeholder-200x200.png"
-    },
-    {
-      imgUrl:"https://pradeepsuthar.github.io/hesley_website_australia/assets/images/placeholder-200x200.png"
-    },
-    {
-      imgUrl:"https://pradeepsuthar.github.io/hesley_website_australia/assets/images/placeholder-200x200.png"
-    },
-    {
-      imgUrl:"https://pradeepsuthar.github.io/hesley_website_australia/assets/images/placeholder-200x200.png"
-    },
-    {
-      imgUrl:"https://pradeepsuthar.github.io/hesley_website_australia/assets/images/placeholder-200x200.png"
-    },
-    {
-      imgUrl:"https://pradeepsuthar.github.io/hesley_website_australia/assets/images/placeholder-200x200.png"
-    },
-  ]
+  videoPlayerwidthSize = 0;
+  videoPlayerheightSize = 0;
 
-  constructor() { }
+  contactForm = new FormGroup({
+    fullName: new FormControl(''),
+    mobileNumber: new FormControl(''),
+    message: new FormControl(''),
+  });
 
-  ngOnInit(): void { }
+  onSubmit(){
+    console.log("Contact Form is",this.contactForm.value)
+    this._themeService.makeEnquiry(this.contactForm.value.fullName,this.contactForm.value.mobileNumber,this.contactForm.value.message)
+    this.contactForm.reset()
+  }
+
+  ngOnInit(): void {
+    console.log("Width : ",window.innerWidth)
+    console.log("Type Width : ",typeof(window.innerWidth))
+    if (window.innerWidth > 1020) {
+      this.videoPlayerwidthSize = 350;
+      this.videoPlayerheightSize = 200;
+    }else if (window.innerWidth > 320) {
+      this.videoPlayerwidthSize = 350;
+      this.videoPlayerheightSize = 200;
+    }else{
+      this.videoPlayerwidthSize = 260;
+      this.videoPlayerheightSize = 200;
+    }
+  }
 
 }
